954e0b24d8c91a648ce11d9c24eacc9e685af045
Mauricio Mercado <me@maumercado.com>
2021-05-19 15:30:09 -0700

    initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import MarketingApp from './components/MarketingApp'
+export default () => {
+  return <div>
+    <h1>Hi there ---- Container</h1>
+    <hr></hr>
+    <MarketingApp />
+  </div>
+}
