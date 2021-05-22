b502d8790afc16fa849480326d000c7c29a3b5c4
Mauricio Mercado <me@maumercado.com>
2021-05-22 12:39:43 -0700

    fix navigation for apps in development

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
-const mount = (el, { onNavigate }) => {
-  const history = createMemoryHistory()
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
 
   if (onNavigate && typeof onNavigate === 'function') {
     history.listen(onNavigate)
@@ -26,10 +26,11 @@ const mount = (el, { onNavigate }) => {
   }
 }
 
+// Are we in development and in isolation
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root')
   if (devRoot) {
-    mount(devRoot, {})
+    mount(devRoot, { defaultHistory: createBrowserHistory() })
   }
 }
 
