53e0eee15b3b1b7b31fdc5fcc88fa477df6b3c45
Mauricio Mercado <me@maumercado.com>
2021-05-25 11:30:06 -0700

    added auth to container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
+import AuthApp from './components/AuthApp'
 import Header from './components/Header'
 
 const generateClassName = createGenerateClassName({
@@ -14,7 +15,10 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route path="/auth" component={AuthApp}></Route>
+            <Route path="/" component={MarketingApp}></Route>
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>
---
+++ packages/container/src/components/AuthApp.js
@@ -0,0 +1,25 @@
+import React, { useRef, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+import { mount } from 'auth/AuthApp'
+
+export default () => {
+  const ref = useRef(null)
+  const history = useHistory()
+
+  function onNavigate ({ pathname: nextPathName }) {
+    const { pathname } = history.location
+    if (pathname !== nextPathName) {
+      history.push(nextPathName)
+    }
+  }
+
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: onNavigate.bind(this)
+    })
+
+    history.listen(onParentNavigate)
+  }, [])
+
+  return <div ref={ref} />
+}
