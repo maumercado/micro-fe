b194de94f4e201041e8e0deb04bb8b9ff1eb6c06
Mauricio Mercado <me@maumercado.com>
2021-05-25 18:17:16 -0700

    triggering dashboard worflow

--- packages/dashboard/src/components/Dashboard.vue
+++ packages/dashboard/src/components/Dashboard.vue
@@ -222,20 +222,20 @@
 </template>
 
 <script>
-import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/primevue.css';
-import 'primeflex/primeflex.css';
-import 'primeicons/primeicons.css';
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.css'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
 
-import Checkbox from 'primevue/checkbox';
-import Panel from 'primevue/panel';
-import Dropdown from 'primevue/dropdown';
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-import Column from 'primevue/column';
-import DataTable from 'primevue/datatable';
-import Chart from 'primevue/chart';
-import ProgressBar from 'primevue/progressbar';
+import Checkbox from 'primevue/checkbox'
+import Panel from 'primevue/panel'
+import Dropdown from 'primevue/dropdown'
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import Column from 'primevue/column'
+import DataTable from 'primevue/datatable'
+import Chart from 'primevue/chart'
+import ProgressBar from 'primevue/progressbar'
 
 export default {
   components: {
