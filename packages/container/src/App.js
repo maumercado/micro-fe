5dad8964082681b8d21fcb7c3b0cf378c7bce152
Mauricio Mercado <me@maumercado.com>
2021-05-25 18:02:15 -0700

    add better flow for login and logout with dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,5 +1,6 @@
-import React, { lazy, Suspense, useState } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { lazy, Suspense, useState, useEffect } from 'react'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 import Header from './components/Header'
@@ -13,11 +14,19 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 })
 
+const history = createBrowserHistory()
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
@@ -26,12 +35,15 @@ export default () => {
               <Route path='/auth'>
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
-              <Route path='/dashboard' component={DashboardLazy} />
+              <Route path='/dashboard'>
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
               <Route path='/' component={MarketingLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   )
 }
